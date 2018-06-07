381333e7d1f77bf04e25699044e29cb80afd2ed4
Brett Cannon <brcan@microsoft.com>
2018-06-07 13:53:36 -0700

    Touch up comments and parameter names

--- tpn/tpn/__main__.py
+++ tpn/tpn/__main__.py
@@ -12,20 +12,21 @@ import pathlib
 import docopt
 
 from . import manual
-from . import npm as npmtools
+from . import npm
 
 
-def main(tpn_path, *, npm=None, pypi=None):
+def main(tpn_path, *, npm_path=None, pypi_path=None):
     tpn_path = pathlib.Path(tpn_path)
     licenses = {}
     if tpn_path.exists():
         known_licenses = manual.parse_license(tpn_path.read_text())
     else:
         known_licenses = {}
-    if npm:
-        with open(npm) as file:
+    # XXX manually-specified licenses
+    if npm_path:
+        with open(npm_path) as file:
             package_data = json.load(file)
-        npm_projects = npmtools.projects(package_data)
+        npm_projects = npm.projects(package_data)
         for name, details in list(npm_projects.items()):
             if name in known_licenses:
                 known_details = known_licenses[name]
@@ -33,10 +34,10 @@ def main(tpn_path, *, npm=None, pypi=None):
                     licenses[name] = known_details
                     del npm_projects[name]
         for name, details in npm_projects:
-            details["license"] = npmtools.fetch_license(details["url"])
-            # XXX fill_in_licenses() could be made concurrent
+            details["license"] = npm.fetch_license(details["url"])
+            # XXX ! fill_in_licenses() which could be made concurrent
             # XXX ! warn if copyleft
-    if pypi:
+    if pypi_path:
         # XXX ! Repeat above for PyPI.
         pass
     # XXX Generate TPN.
@@ -44,4 +45,4 @@ def main(tpn_path, *, npm=None, pypi=None):
 
 if __name__ == "__main__":
     arguments = docopt.docopt(__doc__)
-    main(**arguments)
+    main(arguments["<tpn_path>"], npm_path=arguments["--npm"], pypi_path=arguments["--pypi"])
